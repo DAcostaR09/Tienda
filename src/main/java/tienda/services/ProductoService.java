37722dfa59cc47ca1a79a44d2f581769a02280b3
DAcostaR09 <danirolox09@gmail.com>
2025-10-28 21:04:03 -0600

    semana 8

--- src/main/java/tienda/services/ProductoService.java
+++ src/main/java/tienda/services/ProductoService.java
@@ -1,11 +1,10 @@
-package tienda.services;
+package com.tienda.service;
 
-import tienda.domain.Producto;
-import tienda.repository.ProductoRepository;
+import com.tienda.domain.Producto;
+import com.tienda.repository.ProductoRepository;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,24 +13,26 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class ProductoService {
 
-    @Autowired
-    private ProductoRepository productoRepository;
-    
-    @Transactional(readOnly=true)
+    private final ProductoRepository productoRepository;
+    private final FirebaseStorageService firebaseStorageService;
+
+    public ProductoService(ProductoRepository productoRepository, FirebaseStorageService firebaseStorageService) {
+        this.productoRepository = productoRepository;
+        this.firebaseStorageService = firebaseStorageService;
+    }
+
+    @Transactional(readOnly = true)
     public List<Producto> getProductos(boolean activo) {
         if (activo) {
             return productoRepository.findByActivoTrue();
         }
         return productoRepository.findAll();
     }
-    
+
     @Transactional(readOnly = true)
     public Optional<Producto> getProducto(Integer idProducto) {
         return productoRepository.findById(idProducto);
     }
-
-    @Autowired
-    private FirebaseStorageService firebaseStorageService;
 
     @Transactional
     public void save(Producto producto, MultipartFile imagenFile) {
@@ -51,16 +52,32 @@ public class ProductoService {
 
     @Transactional
     public void delete(Integer idProducto) {
-        // Verifica si el producto existe antes de intentar eliminarlo
+        // Verifica si la categoría existe antes de intentar eliminarlo
         if (!productoRepository.existsById(idProducto)) {
             // Lanza una excepción para indicar que el usuario no fue encontrado
-            throw new IllegalArgumentException("El producto con ID " + idProducto + " no existe.");
+            throw new IllegalArgumentException("La categoría con ID " + idProducto + " no existe.");
         }
         try {
             productoRepository.deleteById(idProducto);
         } catch (DataIntegrityViolationException e) {
             // Lanza una nueva excepción para encapsular el problema de integridad de datos
-            throw new IllegalStateException("No se puede eliminar el producto. Tiene datos asociados.", e);
+            throw new IllegalStateException("No se puede eliminar la producto. Tiene datos asociados.", e);
         }
     }
+
+    @Transactional(readOnly = true)
+    public List<Producto> consultaDerivada(double precioInf, double precioSup) {
+        return productoRepository.findByPrecioBetweenOrderByPrecioAsc(precioInf, precioSup);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Producto> consultaJPQL(double precioInf, double precioSup) {
+        return productoRepository.consultaJPQL(precioInf, precioSup);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Producto> consultaSQL(double precioInf, double precioSup) {
+        return productoRepository.consultaSQL(precioInf, precioSup);
+    }
+
 }
